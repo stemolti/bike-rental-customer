0d9cdd02dbd3daef194a074dee1b3da805c85cc0
Stefano Agbodan <mystefano95@gmail.com>
2025-06-04 14:34:50 +0200

    Adding public-layout (navbar, footer, router-outlet)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicLayoutComponent
   ],
   imports: [
     BrowserModule,
